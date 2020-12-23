afa33b93d1c424617a740a6a61983969046706e6
Saul Cifuentes 1 <saulmatias94@gmail.com>
2020-12-23 20:03:41 -0300

    Mejora largeStraight y One twos three

--- src/main/java/com/mycompany/generala/Funciones.java
+++ src/main/java/com/mycompany/generala/Funciones.java
@@ -1,2 +1,13 @@
-package com.mycompany.generala;public class Funciones {
+package com.mycompany.generala;
+
+public class Funciones {
+    public static int OneTwosThree(int d1, int d2, int d3, int d4, int d5, int x){
+        int sum=0;
+        if (d1 == x) sum += x;
+        if (d2 == x) sum += x;
+        if (d3 == x) sum += x;
+        if (d4 == x) sum += x;
+        if (d5 == x) sum += x;
+        return sum;
+    }
 }
--- src/main/java/com/mycompany/generala/Generala.java
+++ src/main/java/com/mycompany/generala/Generala.java
@@ -36,36 +36,19 @@ public class Generala {
     }
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
-
-        return sum;
+        Funciones f= new Funciones();
+        return f.OneTwosThree(d1,d2,d3,d4,d5,1);
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
+        Funciones f= new Funciones();
+
+        return f.OneTwosThree(d1,d2,d3,d4,d5,2);
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
+        Funciones f= new Funciones();
+        return f.OneTwosThree(d1,d2,d3,d4,d5,3);
     }
 
     protected int[] dice;
@@ -195,8 +178,22 @@ public class Generala {
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
-    {
+    {   int[] tallies;
+        tallies = new int[6];
+        tallies[d1-1] += 1;
+        tallies[d2-1] += 1;
+        tallies[d3-1] += 1;
+        tallies[d4-1] += 1;
+        tallies[d5-1] += 1;
+        if (tallies[1] == 1 &&
+                tallies[2] == 1 &&
+                tallies[3] == 1 &&
+                tallies[4] == 1 &&
+                tallies[5] == 1)
+            return 20;
         return 0;
+
+
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
