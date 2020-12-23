b86f0dc7d9b1dc57d6790ed78750b329f4706190
Saul Cifuentes 1 <saulmatias94@gmail.com>
2020-12-23 20:20:54 -0300

    Mejora smallStraight y largeStraight

--- src/main/java/com/mycompany/generala/Funciones.java
+++ src/main/java/com/mycompany/generala/Funciones.java
@@ -10,4 +10,22 @@ public class Funciones {
         if (d5 == x) sum += x;
         return sum;
     }
+    public static int SmallLargue(int d1, int d2, int d3, int d4, int d5,int punto, int x){
+        int[] tallies;
+        tallies = new int[6];
+        tallies[d1-1] += 1;
+        tallies[d2-1] += 1;
+        tallies[d3-1] += 1;
+        tallies[d4-1] += 1;
+        tallies[d5-1] += 1;
+        if (    tallies[x] == 1 &&
+                tallies[x+1] == 1 &&
+                tallies[x+2] == 1 &&
+                tallies[x+3] == 1 &&
+                tallies[x+4] == 1)
+            return punto;
+        return 0;
+
+    }
+
 }
